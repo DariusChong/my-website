02e07c794443cda2b4fc26cabb1971c9a89ceeda
ccho0090 <ccho0090@student.monash.edu>
2024-02-13 17:18:09 +1100

    Added Catch-all segment and Not Found Page

--- app/websites/[websiteID]/page.tsx
+++ app/websites/[websiteID]/page.tsx
@@ -1,10 +1,14 @@
+import { notFound } from "next/navigation";
+
 export default function WebsiteDetails({ params }: {
     params: { websiteID: string }
 }) {
+    if (parseInt(params.websiteID) > 1000) {
+        notFound()
+    }
     return (
         <>
             <h1>Details about website {params.websiteID}</h1>
-
         </>
     );
 }
